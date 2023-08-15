a53e6c5496cf0a74e8d62efc0b2694818475072c
KTS Hacx <69856755+ktshacx@users.noreply.github.com>
2023-08-15 21:57:31 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,18 +1,18 @@
-const Zerolang = require('./Zerolang'); // Adjust the path as needed
+const Zerolang = require('./Xerolang'); // Adjust the path as needed
 
-describe('Zerolang', () => {
-    const zerolang = new Zerolang();
+describe('Xerolang', () => {
+    const xerolang = new Xerolang();
 
     test('Tokenization', () => {
         const inputCode = `a = 3; b = 7; c = 8;`;
-        const tokenizedCode = zerolang.tokenize(inputCode);
+        const tokenizedCode = xerolang.tokenize(inputCode);
         const expectedTokens = ['a', 'EQ', "3", 'NEXT', 'b', 'EQ', "7", 'NEXT', 'c', 'EQ', "8", 'NEXT'];
         expect(tokenizedCode).toEqual(expectedTokens);
     });
 
     test('Execution - Basic Operations', () => {
         const tokenizedCode = ['a', 'EQ', "3", 'NEXT', 'b', 'EQ', "7", 'NEXT', 'sum', 'EQ', 'a', 'ADD', 'b', 'NEXT', 'LOG', 'sum', 'NEXT'];
-        const result = zerolang.execute(tokenizedCode);
+        const result = xerolang.execute(tokenizedCode);
         const expectedResult = [10];
         expect(result).toEqual(expectedResult);
     });
